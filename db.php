2e03dde8f2b97bd8848f9169e38e198104f0a412
Venkatesh Gorla <140168875+venkatesh89357@users.noreply.github.com>
2024-04-06 16:06:26 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "venkatesh";
+$password = "Venkatesh12!@";
+$dbname = "student";
+//create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//check connection
+if (!$conn) {
+    die("Connection failed:" . mysqli_connect_error());
+}
+echo "";
+?>
